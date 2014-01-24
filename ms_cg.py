d72fb2386055c29460da152da34d74d36c0e2dfa
Ben Webb <ben@salilab.org>
2014-01-23 23:22:43 -0800

    Use new IMP.core.MSConnectivityRestraint constructor.

--- ms_cg.py
+++ ms_cg.py
@@ -67,7 +67,7 @@ def create_restraints(m, all):
         score = IMP.core.KClosePairsPairScore(
             IMP.core.HarmonicSphereDistancePairScore(0, 1),
             tr)
-        r = IMP.core.MSConnectivityRestraint(score)
+        r = IMP.core.MSConnectivityRestraint(m, score)
         iA = r.add_type([rps[0]])
         iB = r.add_type([rps[1]])
         iC = r.add_type([rps[2]])
